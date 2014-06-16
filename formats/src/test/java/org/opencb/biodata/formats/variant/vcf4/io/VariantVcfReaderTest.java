9509ee5f7e2cf9a715f5866c00795a967254680b
Alejandro Alemán <aaleman@cipf.es>
2014-06-16 15:24:35 +0200

    Add VariantVcfReaderTest. pre() adds sampleNames to source. #4

---
+++ formats/src/test/java/org/opencb/biodata/formats/variant/vcf4/io/VariantVcfReaderTest.java
@@ -0,0 +1,45 @@
+package org.opencb.biodata.formats.variant.vcf4.io;
+
+import org.junit.Test;
+import org.opencb.biodata.formats.variant.io.VariantReader;
+import org.opencb.biodata.models.variant.Variant;
+import org.opencb.biodata.models.variant.VariantSource;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class VariantVcfReaderTest {
+
+    private String inputFile = getClass().getResource("/variant-test-file.vcf.gz").getFile();
+    private VariantSource source = new VariantSource(inputFile, "test", "test", "Test file");
+
+    @Test
+    public void readFile(){
+
+        VariantReader reader = new VariantVcfReader(source, inputFile);
+
+        List<Variant> variants;
+
+        assertTrue(reader.open());
+        assertTrue(reader.pre());
+
+        int i = 0;
+        do {
+            variants = reader.read();
+            if(variants != null){
+                i+=variants.size();
+            }
+        } while (variants != null);
+
+        assertEquals(i, 999);
+
+        assertTrue(reader.post());
+        assertTrue(reader.close());
+
+    }
+
+}
