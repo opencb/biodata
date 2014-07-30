69413415a04d71223c173c475453115e0ee96e10
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-07-30 11:31:32 +0100

    Many new fields added to the VariantStudy class

--- models/src/main/java/org/opencb/biodata/models/variant/VariantStudy.java
+++ models/src/main/java/org/opencb/biodata/models/variant/VariantStudy.java
@@ -1,6 +1,5 @@
 package org.opencb.biodata.models.variant;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,13 +8,21 @@ import java.util.List;
  */
 public class VariantStudy {
     
-    private String studyName;
+    private String name;
     
-    private String studyId;
+    private String id;
     
     private String description;
     
-    private String species;
+    private int taxonomyId;
+    
+    private String speciesCommonName;
+    
+    private String speciesScientificName;
+    
+    private String sourceType;
+    
+    private String center;
     
     private String material;
     
@@ -23,8 +30,17 @@ public class VariantStudy {
     
     private String type;
     
+    private String assembly;
+    
+    private String platform;
+    
+    private int numVariants;
+    
+    private int numSamples;
+    
     private List<VariantSource> sources;
 
+    
     public VariantStudy() {
         this(null, null);
     }
@@ -34,35 +50,44 @@ public class VariantStudy {
     }
 
     public VariantStudy(String studyName, String studyId, List<VariantSource> sources) {
-        this(studyName, studyId, sources, null, null, null, null, null);
+        this(studyName, studyId, sources, null, -1, null, null, null, null, null, null, null, null, null, -1, -1);
     }
 
     public VariantStudy(String studyName, String studyId, List<VariantSource> sources, String description, 
-            String species, String material, String scope, String type) {
-        this.studyName = studyName;
-        this.studyId = studyId;
+            int speciesId, String speciesCommonName, String speciesScientificName, String sourceType, String center, String material, 
+            String scope, String type, String referenceAssembly, String platform, int numVariants, int numSamples) {
+        this.name = studyName;
+        this.id = studyId;
         this.description = description;
-        this.species = species;
+        this.taxonomyId = speciesId;
+        this.speciesCommonName = speciesCommonName;
+        this.speciesScientificName = speciesScientificName;
+        this.sourceType = sourceType;
+        this.center = center;
         this.material = material;
         this.scope = scope;
         this.type = type;
-        this.sources = sources != null ? sources : new ArrayList();
+        this.assembly = referenceAssembly;
+        this.platform = platform;
+        this.numVariants = numVariants;
+        this.numSamples = numSamples;
+        this.sources = sources;
+    }
+    
+    public String getName() {
+        return name;
     }
 
-    public String getStudyName() {
-        return studyName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setStudyName(String studyName) {
-        this.studyName = studyName;
+    public String getId() {
+        return id;
     }
 
-    public String getStudyId() {
-        return studyId;
-    }
-
-    public void setStudyId(String studyId) {
-        this.studyId = studyId;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getDescription() {
@@ -73,12 +98,44 @@ public class VariantStudy {
         this.description = description;
     }
 
-    public String getSpecies() {
-        return species;
+    public int getTaxonomyId() {
+        return taxonomyId;
     }
 
-    public void setSpecies(String species) {
-        this.species = species;
+    public void setTaxonomyId(int taxonomyId) {
+        this.taxonomyId = taxonomyId;
+    }
+
+    public String getSpeciesCommonName() {
+        return speciesCommonName;
+    }
+
+    public void setSpeciesCommonName(String speciesCommonName) {
+        this.speciesCommonName = speciesCommonName;
+    }
+
+    public String getSpeciesScientificName() {
+        return speciesScientificName;
+    }
+
+    public void setSpeciesScientificName(String speciesScientificName) {
+        this.speciesScientificName = speciesScientificName;
+    }
+
+    public String getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(String sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public String getCenter() {
+        return center;
+    }
+
+    public void setCenter(String center) {
+        this.center = center;
     }
 
     public String getMaterial() {
@@ -103,6 +160,38 @@ public class VariantStudy {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getAssembly() {
+        return assembly;
+    }
+
+    public void setAssembly(String assembly) {
+        this.assembly = assembly;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public int getNumVariants() {
+        return numVariants;
+    }
+
+    public void setNumVariants(int numVariants) {
+        this.numVariants = numVariants;
+    }
+
+    public int getNumSamples() {
+        return numSamples;
+    }
+
+    public void setNumSamples(int numSamples) {
+        this.numSamples = numSamples;
     }
 
     public List<VariantSource> getSources() {
