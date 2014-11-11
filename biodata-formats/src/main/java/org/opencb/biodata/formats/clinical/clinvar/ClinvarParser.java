7171095ea5eafce05d9756904576a43e4a0e4f2a
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-11-11 11:27:10 +0100

    moved clinical classes to new package biodata/formats/clinical

---
+++ biodata-formats/src/main/java/org/opencb/biodata/formats/clinical/clinvar/ClinvarParser.java
@@ -0,0 +1,49 @@
+package org.opencb.biodata.formats.clinical.clinvar;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class ClinvarParser {
+
+    public final static String CLINVAR_CONTEXT_v19 = "org.opencb.biodata.formats.clinical.clinvar.v19jaxb";
+
+    public static void saveXMLInfo(Object obj, String filename) throws FileNotFoundException, JAXBException {
+        JAXBContext jaxbContext;
+        jaxbContext = JAXBContext.newInstance(CLINVAR_CONTEXT_v19);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.marshal(obj, new FileOutputStream(filename));
+    }
+
+    /**
+     * Checks if XML info path exists and loads it
+     *
+     * @throws javax.xml.bind.JAXBException
+     * @throws java.io.IOException
+     */
+    public static Object loadXMLInfo(String filename) throws JAXBException {
+        Object obj = null;
+        JAXBContext jaxbContext = JAXBContext.newInstance(CLINVAR_CONTEXT_v19);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        obj = unmarshaller.unmarshal(new File(filename));
+        return obj;
+    }
+
+    /**
+     * Checks if XML info path exists and loads it
+     *
+     * @throws javax.xml.bind.JAXBException
+     * @throws java.io.IOException
+     */
+    public static Object loadXMLInfo(String filename, String clinvarVersion) throws JAXBException {
+        Object obj = null;
+        JAXBContext jaxbContext = JAXBContext.newInstance(clinvarVersion);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        obj = unmarshaller.unmarshal(new File(filename));
+        return obj;
+    }
+}
