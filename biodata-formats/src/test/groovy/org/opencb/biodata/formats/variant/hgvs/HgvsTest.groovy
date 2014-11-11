9886090ba23632f1ed47e6c0b461509bb3a8cfed
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-11-11 09:39:26 +0100

    New hgvs tests

--- biodata-formats/src/test/groovy/org/opencb/biodata/formats/variant/hgvs/HgvsTest.groovy
+++ biodata-formats/src/test/groovy/org/opencb/biodata/formats/variant/hgvs/HgvsTest.groovy
@@ -25,7 +25,7 @@ class HgvsTest extends spock.lang.Specification {
     }
 
     @Unroll
-    def "variant of #hgvs should be #chr:#start:#reference->#alternate"() {
+    def "variant corresponding to HGVS #hgvs should be #chr:#start:#reference -> #alternate"() {
         when: "obtain the hgvs location"
         Variant variant = new Hgvs(hgvs).getVariant(genomeSequenceFastaFile)
 
@@ -36,18 +36,20 @@ class HgvsTest extends spock.lang.Specification {
         variant.getReference() == reference
         variant.getAlternate() == alternate
 
-        where:
-        hgvs                          || chr   | start  | end | reference | alternate
-        "NC_000001.9:g.35G>A"         || "1"   | 35     | 35  | "G"       | "A"
-        "NC_000014.9:g.35insA"        || "14"  | 35     | 35  | "-"       | "A"
-        "NC_000024.10:g.35delG"       || "Y"   | 35     | 35  | "G"       | "-"
-        "NC_000019.9:g.14del"         || "19"  | 14     | 14  | "C"       | "-"
-        and: "one nucleotide duplication is treated like one insertion"
-        "NC_000019.9:g.14dupC"        || "19"  | 14     | 14  | "-"       | "C"
-        "NC_000019.9:g.14dup"         || "19"  | 14     | 14  | "-"       | "C"
+        where: "single nucleotide mutations"
+        hgvs                           || chr   | start  | end    | reference | alternate
+        "NC_000001.11:g.374354G>A"     || "1"   | 374354 | 374354 | "G"       | "A"
+        "NC_000014.9:g.481_482insA"    || "14"  | 481    | 482    | "-"       | "A"
+        "NC_000024.10:g.35delG"        || "Y"   | 35     | 35     | "G"       | "-"
+        "NC_000019.10:g.14del"         || "19"  | 14     | 14     | "C"       | "-"
+        "NC_000017.11:g.46739dupC"     || "17"  | 46739  | 46739  | "-"       | "C"
+        "NC_000019.10:g.14dup"         || "19"  | 14     | 14     | "-"       | "C"
         and: "multiple nucleotide indels"
-        "NC_000004.8:g.692_694delGAC" || "4"   | 692    | 694 | "GAC"     | "-"
-        "NC_000019.9:g.34_36del"      || "19"  | 34     | 36  | "TGC"     | "-"
+        "NC_000023.11:g.692_694delGAC" || "X"   | 692    | 694    | "GAC"     | "-"
+        "NC_000019.10:g.34_36del"      || "19"  | 34     | 36     | "TGC"     | "-"
+        "NC_000007.14:g.792_793insGAG" || "7"   | 792    | 793    | "-"       | "GAG"
+        "NC_000002.12:g.92_94dupGAC"   || "2"   | 92     | 94     | "-"       | "GAC"
+        "NC_000019.10:g.34_36dup"      || "19"  | 34     | 36     | "-"       | "TGC"
     }
 
     @Unroll
@@ -71,6 +73,6 @@ class HgvsTest extends spock.lang.Specification {
         variant == null
 
         where:
-        hgvs << ["NC_000016.10:g.2088677_2088679delTGAinsT"]
+        hgvs << ["NC_000016.10:g.2088677_2088679delTGAinsT", "NC_000019.9:c.14ins"]
     }
 }
