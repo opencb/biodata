7d47c0f1c0e41c39f67e16d0781e8f7937b0ba24
Javi Lopez <fjlopez@ebi.ac.uk>
2014-11-22 11:03:25 +0000

    feature/0.3.x: bug solved in ConsequenceType

--- biodata-models/src/main/java/org/opencb/biodata/models/variant/annotation/ConsequenceTypeMappings.java
+++ biodata-models/src/main/java/org/opencb/biodata/models/variant/annotation/ConsequenceTypeMappings.java
@@ -116,7 +116,7 @@ public class ConsequenceTypeMappings {
         accessionToTerm.put(336, "polypeptide_variation_site");
     }
 
-    public int getAccession(String SOName) {
-        return termToAccession.get(SOName);
-    }
+//    public int getAccession(String SOName) {
+//        return termToAccession.get(SOName);
+//    }
 }
