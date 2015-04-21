a10bbb64d84bf4fb0f4749a5980487d7ec3d2742
Javi Lopez <javild@gmail.com>
2015-04-21 11:28:50 +0100

    conflicts solved when merging from master

--- biodata-formats/src/test/java/org/opencb/biodata/formats/annotation/io/VepFormatReaderTest.java
+++ biodata-formats/src/test/java/org/opencb/biodata/formats/annotation/io/VepFormatReaderTest.java
@@ -1,6 +1,9 @@
 package org.opencb.biodata.formats.annotation.io;
 
 import org.junit.Test;
+import org.opencb.biodata.models.variant.annotation.VariantAnnotation;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,10 +11,15 @@ public class VepFormatReaderTest {
 
     @Test
     public void testRead() throws Exception {
-        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test.vep");
+//        VepFormatReader vepFormatReader = new VepFormatReader("/home/fjlopez/EBI/eva/data/clinvar.vep");
+        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/clinvar.vep");
+//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/vep22.vep");
+//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test.vep");
+//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/vep22.head.tsv");
+//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test1.vep");
         vepFormatReader.open();
         vepFormatReader.pre();
-        vepFormatReader.read(3);
+        List<VariantAnnotation> variantAnnotationList = vepFormatReader.read(10000);
         vepFormatReader.post();
         vepFormatReader.close();
     }
