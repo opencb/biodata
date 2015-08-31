d1020df7926fa10156360d97676d470e07cd2fbf
Milan Simonovic <mbsimonovic@gmail.com>
2015-08-31 22:42:49 +0200

    fix for https://github.com/opencb/biodata/issues/52

---
+++ biodata-formats/src/test/java/org/opencb/biodata/formats/feature/gtf/io/GtfReaderTest.java
@@ -0,0 +1,33 @@
+package org.opencb.biodata.formats.feature.gtf.io;
+
+import org.junit.Test;
+import org.opencb.biodata.formats.feature.gtf.Gtf;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Milan Simonovic <milan.simonovic@imls.uzh.ch>
+ */
+public class GtfReaderTest {
+
+    @Test
+    public void whitespaceAttributeParseBug() throws Exception {
+        String gtfRecord = "4\tensembl_havana\tgene\t7392435\t7442901\t.\t+\t.\tgene_id \"ENSDARG00000076014\"; gene_version \"3\"; gene_name \"ERC1 (3 of 3)\"; gene_source \"ensembl_havana\"; gene_biotype \"protein_coding\";";
+
+        Map<String, String> attributes = new HashMap<String, String>() {{
+            put("gene_version", "3");
+            put("gene_name", "ERC1 (3 of 3)");
+            put("gene_source", "ensembl_havana");
+            put("gene_biotype", "protein_coding");
+        }};
+        final Gtf expected = new Gtf("4", "ensembl_havana", "gene", 7392435, 7442901, "+", ".", ".", attributes);
+
+        final GtfReader gtfReader = new GtfReader(new StringReader(gtfRecord));
+        final Gtf actual = gtfReader.read();
+        assertEquals(expected, actual);
+    }
+}
