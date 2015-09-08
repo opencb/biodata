e3d2593ad1132d0683ff04063298a109dcaa66ed
kalyanreddyemani <kalyanreddye@gmail.com>
2015-09-08 16:49:59 +0100

    VariantContextToVariantConverter is modified with some comments

---
+++ biodata-models/src/main/java/org/opencb/biodata/models/variant/avro/AvroToVariantContextBean.java
@@ -0,0 +1,140 @@
+package org.opencb.biodata.models.variant.avro;
+
+import java.util.Set;
+
+public class AvroToVariantContextBean {
+	
+	
+	public String chrom;
+	public int pos;
+	public String id;
+	public String ref;
+	public String alt;
+	public String qual;
+	public String filter;
+	public String info;
+	public String format;
+	public String sample1;
+	/**
+	 * @return the chrom
+	 */
+	public String getChrom() {
+		return chrom;
+	}
+	/**
+	 * @param chrom the chrom to set
+	 */
+	public void setChrom(String chrom) {
+		this.chrom = chrom;
+	}
+	/**
+	 * @return the pos
+	 */
+	public int getPos() {
+		return pos;
+	}
+	/**
+	 * @param pos the pos to set
+	 */
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the ref
+	 */
+	public String getRef() {
+		return ref;
+	}
+	/**
+	 * @param ref the ref to set
+	 */
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	/**
+	 * @return the alt
+	 */
+	public String getAlt() {
+		return alt;
+	}
+	/**
+	 * @param alt the alt to set
+	 */
+	public void setAlt(String alt) {
+		this.alt = alt;
+	}
+	/**
+	 * @return the qual
+	 */
+	public String getQual() {
+		return qual;
+	}
+	/**
+	 * @param qual the qual to set
+	 */
+	public void setQual(String qual) {
+		this.qual = qual;
+	}
+	/**
+	 * @return the filter
+	 */
+	public String getFilter() {
+		return filter;
+	}
+	/**
+	 * @param filter the filter to set
+	 */
+	public void setFilter(String filter) {
+		this.filter = filter;
+	}
+	/**
+	 * @return the info
+	 */
+	public String getInfo() {
+		return info;
+	}
+	/**
+	 * @param info the info to set
+	 */
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	/**
+	 * @return the format
+	 */
+	public String getFormat() {
+		return format;
+	}
+	/**
+	 * @param format the format to set
+	 */
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	/**
+	 * @return the sample1
+	 */
+	public String getSample1() {
+		return sample1;
+	}
+	/**
+	 * @param sample1 the sample1 to set
+	 */
+	public void setSample1(String sample1) {
+		this.sample1 = sample1;
+	}
+	
+	
+}
