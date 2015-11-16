b2cad606dae398891ad037c4ea77e90c6240a7da
Javi Lopez <javild@gmail.com>
2015-11-16 16:58:15 +0000

    develop: additionalAttributes field within VariantAnnotation can now take more Map types

--- biodata-tools/src/main/java/org/opencb/biodata/tools/variant/converter/VariantContextToVariantConverter.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/variant/converter/VariantContextToVariantConverter.java
@@ -364,7 +364,7 @@ public class VariantContextToVariantConverter implements Converter<VariantContex
         /*
          * set AdditionalAttributes map type parameter
          */
-        Map<String, String> additionalAttributesMap = new HashMap();
+        Map<String, Object> additionalAttributesMap = new HashMap();
         //additionalAttributesMap.put(null, null);
         variantAnnotation.setAdditionalAttributes(additionalAttributesMap);
         /*
