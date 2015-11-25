32f12742719804e6f190032f987df29f80bc7546
Javi Lopez <javild@gmail.com>
2015-11-25 14:23:37 +0000

    hotfix/0.4: VepFormatReader can now handle chromosome ids containing _ Corresponding test added as well. Solves #51 adn #48

--- biodata-formats/src/main/java/org/opencb/biodata/formats/annotation/io/VepFormatReader.java
+++ biodata-formats/src/main/java/org/opencb/biodata/formats/annotation/io/VepFormatReader.java
@@ -1,5 +1,6 @@
 package org.opencb.biodata.formats.annotation.io;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.opencb.biodata.formats.protein.uniprot.v135jaxb.InteractantType;
 import org.opencb.biodata.formats.variant.vcf4.io.VariantVcfReader;
 import org.opencb.biodata.models.variant.annotation.ConsequenceType;
@@ -388,9 +389,16 @@ public class VepFormatReader implements DataReader<VariantAnnotation> {
             String[] variantFields = variantString.split("[\\/]");
             //        String[] variantFields = variantString.split("[\\_\\/]");
             String[] leftVariantFields = variantFields[0].split("_");
-            parsedVariant.put("chromosome", leftVariantFields[0]);
-            parsedVariant.put("start", leftVariantFields[1]);
-            parsedVariant.put("reference", leftVariantFields[2]);
+
+            // Chr id containing _
+            if(leftVariantFields.length>3) {
+                parsedVariant.put("chromosome",
+                        String.join("_", (String[]) ArrayUtils.subarray(leftVariantFields, 0, leftVariantFields.length - 2)));
+            } else {
+                parsedVariant.put("chromosome", leftVariantFields[0]);
+            }
+            parsedVariant.put("start", leftVariantFields[leftVariantFields.length-2]);
+            parsedVariant.put("reference", leftVariantFields[leftVariantFields.length-1]);
             parsedVariant.put("alternative", variantFields[1]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Unexpected variant format for column 1: "+variantString);
--- biodata-formats/src/test/java/org/opencb/biodata/formats/annotation/io/VepFormatReaderTest.java
+++ biodata-formats/src/test/java/org/opencb/biodata/formats/annotation/io/VepFormatReaderTest.java
@@ -11,15 +11,27 @@ public class VepFormatReaderTest {
 
     @Test
     public void testRead() throws Exception {
-//        VepFormatReader vepFormatReader = new VepFormatReader("/home/fjlopez/EBI/eva/data/clinvar.vep");
-        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test.vep");
-//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/vep22.vep");
-//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test.vep");
-//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/vep22.head.tsv");
-//        VepFormatReader vepFormatReader = new VepFormatReader("/tmp/test1.vep");
+        VepFormatReader vepFormatReader = new VepFormatReader(getClass().getResource("/vepoutputtest.tsv.gz").getFile());
         vepFormatReader.open();
         vepFormatReader.pre();
         List<VariantAnnotation> variantAnnotationList = vepFormatReader.read(10000);
+        assertEquals(variantAnnotationList.size(),2);
+        assertEquals(variantAnnotationList.get(0).getChromosome(), "1");
+        assertEquals(variantAnnotationList.get(0).getStart(), 628314);
+        assertEquals(variantAnnotationList.get(0).getEnd(), 628332);
+        assertEquals(variantAnnotationList.get(0).getReferenceAllele(), "CAGGTGACACTGGGGACAC");
+        assertEquals(variantAnnotationList.get(0).getAlternativeAllele(), "-");
+        assertEquals(variantAnnotationList.get(0).getConsequenceTypes().size(), 1);
+        assertEquals(variantAnnotationList.get(0).getConsequenceTypes().get(0).getSoTerms().size(), 1);
+        assertEquals(variantAnnotationList.get(0).getConsequenceTypes().get(0).getSoTerms().get(0).getSoName(), "intron_variant");
+        assertEquals(variantAnnotationList.get(1).getChromosome(), "LGE22C19W28_E50C23");
+        assertEquals(variantAnnotationList.get(1).getStart(), 351697);
+        assertEquals(variantAnnotationList.get(1).getEnd(), 351697);
+        assertEquals(variantAnnotationList.get(1).getReferenceAllele(), "-");
+        assertEquals(variantAnnotationList.get(1).getAlternativeAllele(), "AT");
+        assertEquals(variantAnnotationList.get(1).getConsequenceTypes().size(), 1);
+        assertEquals(variantAnnotationList.get(1).getConsequenceTypes().get(0).getSoTerms().size(), 1);
+        assertEquals(variantAnnotationList.get(1).getConsequenceTypes().get(0).getSoTerms().get(0).getSoName(), "intergenic_variant");
         vepFormatReader.post();
         vepFormatReader.close();
     }
