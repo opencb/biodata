0d7d685f18e683174e7b7db25ea079f369e61d67
Javier Lopez <javild@gmail.com>
2016-09-02 15:10:35 +0100

    hotfix-0.8: VariantAnnotation reference should not be copied when doing VariantNormalization - if any, the object must be properly cloned within the new Variant object. Otherwise all new Variants created from an MNV or multiallelic positions will be pointing to the same VariantAnnotation object and this causes problems. For now, the VariantAnnotation object is no longer going to be copied/cloned - if a set of variants is generated from a MNV or a multiallelic position the annotation may not be the same for all of them and shall be recalculated. The VariantNormalizer user must create the new VariantAnnotation objects

--- biodata-models/src/main/java/org/opencb/biodata/models/variant/VariantNormalizer.java
+++ biodata-models/src/main/java/org/opencb/biodata/models/variant/VariantNormalizer.java
@@ -929,7 +929,7 @@ public class VariantNormalizer implements ParallelTaskRunner.Task<Variant, Varia
         Variant normalizedVariant = new Variant(variant.getChromosome(), keyFields.getStart(), keyFields.getEnd(), keyFields.getReference(), keyFields.getAlternate());
         normalizedVariant.setIds(variant.getIds());
         normalizedVariant.setStrand(variant.getStrand());
-        normalizedVariant.setAnnotation(variant.getAnnotation());
+//        normalizedVariant.setAnnotation(variant.getAnnotation());
         if (variant.getStudies() != null && !variant.getStudies().isEmpty()) {
             if (variant.getStudies().get(0).getAllAttributes().containsKey(CIPOS_STRING)) {
                 int[] impreciseStart = getImpreciseStart(variant);
