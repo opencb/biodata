55bc2be6ad68f84e30d8f1ee3c27006825e1e153
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2016-11-17 10:04:01 +0100

    tools: add region filters for alignments, closes #126

--- biodata-tools/src/main/java/org/opencb/biodata/tools/alignment/filters/SamRecordFilters.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/alignment/filters/SamRecordFilters.java
@@ -1,7 +1,10 @@
 package org.opencb.biodata.tools.alignment.filters;
 
 import htsjdk.samtools.SAMRecord;
+import org.opencb.biodata.models.core.Region;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -36,6 +39,29 @@ public class SamRecordFilters extends AlignmentFilters<SAMRecord> {
     @Override
     public AlignmentFilters<SAMRecord> addUnmappedFilter() {
         filters.add(samRecord -> samRecord.getReadUnmappedFlag());
+        return this;
+    }
+
+    @Override
+    public AlignmentFilters<SAMRecord> addRegionFilter(Region region, boolean contained) {
+        return addRegionFilter(Arrays.asList(region), contained);
+    }
+
+    @Override
+    public AlignmentFilters<SAMRecord> addRegionFilter(List<Region> regions, boolean contained) {
+        List<Predicate<SAMRecord>> predicates = new ArrayList<>();
+        for (Region region: regions) {
+            if (contained) {
+                predicates.add(samRecord -> samRecord.getReferenceName().equals(region.getChromosome())
+                        && samRecord.getAlignmentStart() >= region.getStart()
+                        && samRecord.getAlignmentEnd() <= region.getEnd());
+            } else {
+                predicates.add(samRecord -> samRecord.getReferenceName().equals(region.getChromosome())
+                        && samRecord.getAlignmentStart() <= region.getEnd()
+                        && samRecord.getAlignmentEnd() >= region.getStart());
+            }
+        }
+        addFilterList(predicates);
         return this;
     }
 }
--- biodata-tools/src/test/java/org/opencb/biodata/tools/alignment/filters/SamRecordFiltersTest.java
+++ biodata-tools/src/test/java/org/opencb/biodata/tools/alignment/filters/SamRecordFiltersTest.java
@@ -8,6 +8,7 @@ import org.opencb.biodata.tools.alignment.BamManager;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -55,5 +56,24 @@ public class SamRecordFiltersTest  {
         List<SAMRecord> results = BamManager.query(region, options, alignmentFilters);
         System.out.println("Number of results: " + results.size());
         assertEquals(96, results.size());
+    }
+
+    @Test
+    public void testRegion() throws Exception {
+        Path inputPath = Paths.get(getClass().getResource("/HG00096.chrom20.small.bam").toURI());
+        BamManager BamManager = new BamManager(inputPath);
+
+        AlignmentOptions options = new AlignmentOptions().setContained(false);
+        SamRecordFilters alignmentFilters = new SamRecordFilters();
+
+        List<Region> regions = new ArrayList<>();
+        regions.add(new Region("20", 61000, 61500));
+        regions.add(new Region("20", 63000, 63500));
+        alignmentFilters.addRegionFilter(regions, true);
+
+        List<SAMRecord> results = BamManager.query(null, options, alignmentFilters);
+        System.out.println("Number of results: " + results.size());
+        assertEquals(17, results.size()); // contained: 17
+        //assertEquals(96, results.size()); // not contained: 28
     }
 }
