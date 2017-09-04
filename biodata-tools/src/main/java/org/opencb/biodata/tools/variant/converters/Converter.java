cd1badfda4e5bde29c7238d65c80c6b278f4872f
imedina <igmecas@gmail.com>
2017-09-04 15:46:13 +0100

    tools: minor style changes in variant/Converter

--- biodata-tools/src/main/java/org/opencb/biodata/tools/variant/converters/Converter.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/variant/converters/Converter.java
@@ -31,14 +31,14 @@ import java.util.List;
  * @author Matthias Haimel mh719+git@cam.ac.uk
  *
  */
-public interface Converter <FROM,TO> extends ParallelTaskRunner.Task<FROM, TO> {
+public interface Converter<FROM, TO> extends ParallelTaskRunner.Task<FROM, TO> {
 
     TO convert(FROM from);
 
     default List<TO> apply(List<FROM> from) {
         List<TO> convertedBatch = new ArrayList<>(from.size());
-        for (FROM variantContext : from) {
-            convertedBatch.add(convert(variantContext));
+        for (FROM item: from) {
+            convertedBatch.add(convert(item));
         }
         return convertedBatch;
     }
