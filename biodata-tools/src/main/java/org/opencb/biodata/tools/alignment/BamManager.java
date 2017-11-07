cdd618c298bd322e4ee40b79b42d57c5450bea44
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-11-07 16:37:53 +0000

    tools: clean code

--- biodata-tools/src/main/java/org/opencb/biodata/tools/alignment/BamManager.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/alignment/BamManager.java
@@ -41,7 +41,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
