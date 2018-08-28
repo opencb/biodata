5b977b78ab7a16fbea63ec1c849e9f1b72748ed4
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2018-08-28 15:41:22 +0100

    tools: creates a class to resolve modes of inheritance

---
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/pedigree/ModeOfInheritance.java
@@ -0,0 +1,26 @@
+package org.opencb.biodata.tools.pedigree;
+
+import org.opencb.biodata.models.commons.Phenotype;
+import org.opencb.biodata.models.core.pedigree.Pedigree;
+
+import java.util.List;
+import java.util.Map;
+
+public class ModeOfInheritance {
+
+    public static Map<String, List<String>> dominant(Pedigree pedigree, Phenotype phenotype) {
+        return null;
+    }
+
+    public static Map<String, List<String>> recessive(Pedigree pedigree, Phenotype phenotype) {
+        return null;
+    }
+
+    public static Map<String, List<String>> xLinked(Pedigree pedigree, Phenotype phenotype) {
+        return null;
+    }
+
+    public static Map<String, List<String>> yLinked(Pedigree pedigree, Phenotype phenotype) {
+        return null;
+    }
+}
