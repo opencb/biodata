9ad414b6136dd146742a2a09b74012b37e40d0f1
Jacobo Coll Moragón <jacobo167@gmail.com>
2018-10-12 17:00:48 +0100

    tools: Add new filters to VariantAvroFilters

--- biodata-tools/src/main/java/org/opencb/biodata/tools/commons/CommonsFilters.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/commons/CommonsFilters.java
@@ -19,34 +19,49 @@
 
 package org.opencb.biodata.tools.commons;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
 import java.util.function.Predicate;
 
 /**
  * Created by joaquin on 11/14/16.
  */
-public class CommonsFilters<T> {
+public class CommonsFilters<T> implements Predicate<T> {
 
     protected List<Predicate<T>> filters;
+    protected boolean mustPassAll;
 
     public CommonsFilters() {
         this(new ArrayList<>());
     }
 
     public CommonsFilters(List<Predicate<T>> filters) {
-        this.filters = filters;
+        this(filters, true);
     }
 
+    public CommonsFilters(List<Predicate<T>> filters, boolean mustPassAll) {
+        this.filters = filters == null ? new ArrayList<>() : filters;
+        this.mustPassAll = mustPassAll;
+    }
+
+    @Override
     public boolean test(T elem) {
-        if (filters != null && filters.size() > 0) {
+        if (mustPassAll) {
             for (Predicate<T> filter : filters) {
                 if (!filter.test(elem)) {
                     return false;
                 }
             }
+            return true;
+        } else {
+            for (Predicate<T> filter : filters) {
+                if (filter.test(elem)) {
+                    return true;
+                }
+            }
+            return false;
         }
-        return true;
     }
 
     public CommonsFilters<T> addFilter(Predicate<T> predicate) {
@@ -85,7 +100,76 @@ public class CommonsFilters<T> {
     }
 
     public CommonsFilters setFilters(List<Predicate<T>> filters) {
-        this.filters = filters;
+        this.filters = filters == null ? new ArrayList<>() : filters;
         return this;
+    }
+
+    public CommonsFilters<T> setMustPassAll(boolean mustPassAll) {
+        this.mustPassAll = mustPassAll;
+        return this;
+    }
+
+    public CommonsFilters<T> setMustPassAny(boolean mustPassAny) {
+        this.mustPassAll = !mustPassAny;
+        return this;
+    }
+
+    protected static String[] splitOperator(String value) {
+        int first = StringUtils.indexOfAny(value, '=', '>', '<');
+        int last = StringUtils.lastIndexOfAny(value, "=", ">", "<");
+
+        if (first == StringUtils.INDEX_NOT_FOUND) {
+            throw new IllegalArgumentException("Malformed filter. Expected <KEY><OP><VALUE>");
+        }
+
+        return new String[]{value.substring(0, first), value.substring(first, last + 1), value.substring(last + 1)};
+    }
+
+    protected static Predicate<String> buildPredicate(String op, String value) {
+        Predicate<String> predicate;
+        Double numValue;
+        switch (op) {
+            case "=":
+            case "==":
+                Set<String> values;
+                if (value.contains(",")) {
+                    values = new HashSet<>(Arrays.asList(value.split(",")));
+                } else {
+                    values = Collections.singleton(value);
+                }
+                predicate = values::contains;
+                break;
+            case ">":
+                numValue = Double.valueOf(value);
+                predicate = v -> {
+                    if (StringUtils.isEmpty(v) || v.equals(".")) {
+                        return false;
+                    } else {
+                        try {
+                            return Double.valueOf(v) > numValue;
+                        } catch (NumberFormatException e) {
+                            return false;
+                        }
+                    }
+                };
+                break;
+            case "<":
+                numValue = Double.valueOf(value);
+                predicate = v -> {
+                    if (StringUtils.isEmpty(v) || v.equals(".")) {
+                        return false;
+                    } else {
+                        try {
+                            return Double.valueOf(v) < numValue;
+                        } catch (NumberFormatException e) {
+                            return false;
+                        }
+                    }
+                };
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported operator " + op);
+        }
+        return predicate;
     }
 }
--- biodata-tools/src/main/java/org/opencb/biodata/tools/variant/filters/VariantAvroFilters.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/variant/filters/VariantAvroFilters.java
@@ -19,15 +19,18 @@
 
 package org.opencb.biodata.tools.variant.filters;
 
+import htsjdk.variant.vcf.VCFConstants;
 import org.opencb.biodata.models.core.Region;
 import org.opencb.biodata.models.variant.StudyEntry;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.avro.FileEntry;
+import org.opencb.biodata.models.variant.avro.VariantType;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
+
+import static org.opencb.biodata.models.variant.StudyEntry.FILTER;
+import static org.opencb.biodata.models.variant.StudyEntry.QUAL;
 
 /**
  * Created by joaquin on 11/14/16.
@@ -37,6 +40,14 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
     private String datasetId;
     private String fileId;
 
+    public VariantAvroFilters() {
+        super();
+    }
+
+    public VariantAvroFilters(Predicate<Variant>... filters) {
+        super(Arrays.asList(filters));
+    }
+
     public VariantAvroFilters(String datasetId, String fileId) {
         super();
         this.datasetId = datasetId;
@@ -44,43 +55,51 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
     }
 
     @Override
-    public VariantFilters<Variant> addTypeFilter(String type) {
-        filters.add(variant -> variant.getType().equals(type));
+    public VariantAvroFilters addTypeFilter(String type) {
+        return addTypeFilter(VariantType.valueOf(type));
+    }
+
+    public VariantAvroFilters addTypeFilter(VariantType variantType) {
+        filters.add(variant -> variant.getType().equals(variantType));
         return this;
     }
 
     @Override
-    public VariantFilters<Variant> addSNPFilter() {
-        filters.add(variant -> !variant.getId().equalsIgnoreCase(".")
-                && !variant.getId().equalsIgnoreCase(""));
+    public VariantAvroFilters addSNPFilter() {
+        filters.add(variant -> !variant.getId().equals(".") && !variant.getId().isEmpty());
+        return this;
+    }
+
+    public VariantAvroFilters addSampleFormatFilter(String formatKey, Predicate<String> valueValidator) {
+        filters.add(variant -> filterSampleFormat(variant, formatKey, true, valueValidator));
         return this;
     }
 
     @Override
-    public VariantFilters<Variant> addQualFilter(double minQual) {
+    public VariantAvroFilters addQualFilter(double minQual) {
         filters.add(variant -> filterQuality(variant, minQual));
         return this;
     }
 
     @Override
-    public VariantFilters<Variant> addPassFilter() {
+    public VariantAvroFilters addPassFilter() {
         return addPassFilter("PASS");
     }
 
     @Override
-    public VariantFilters<Variant> addPassFilter(String name) {
-        filters.add(variant -> filterPass(variant, name));
+    public VariantAvroFilters addPassFilter(String name) {
+        filters.add(variant -> filterFilter(variant, name));
         return this;
     }
 
     @Override
-    public VariantFilters<Variant> addRegionFilter(Region region, boolean contained) {
-        return addRegionFilter(Arrays.asList(region), contained);
+    public VariantAvroFilters addRegionFilter(Region region, boolean contained) {
+        return addRegionFilter(Collections.singletonList(region), contained);
     }
 
     @Override
-    public VariantFilters<Variant> addRegionFilter(List<Region> regions, boolean contained) {
-        List<Predicate<Variant>> predicates = new ArrayList<>();
+    public VariantAvroFilters addRegionFilter(List<Region> regions, boolean contained) {
+        List<Predicate<Variant>> predicates = new ArrayList<>(regions.size());
         for (Region region: regions) {
             if (contained) {
                 predicates.add(variant -> variant.getChromosome().equals(region.getChromosome())
@@ -112,6 +131,107 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
         this.fileId = fileId;
     }
 
+    /**
+     * Add a list of comma semi colon filters. Must pass all filters.
+     * Each filter can be defined with {type}:{key}[>|<|=]{value}
+     * Example:
+     *  FORMAT:GQ>10;INFO:DP>40;FILTER=lowGQ,lowDP
+     *
+     * @param filters          Filters
+     */
+    public VariantAvroFilters addFilter(String filters) {
+        String[] filtersSplit = filters.split(";");
+        return addFilter(filtersSplit);
+    }
+
+    /**
+     * Add a list of filters. Must pass all filters. Each filter can be defined with {type}:{key}[>|<|=]{value}
+     * Example:
+     *  FORMAT:GQ>10
+     *  INFO:DP>40
+     *  FILTER=lowGQ,lowDP
+     *
+     * @param filters          Filters
+     */
+    public VariantAvroFilters addFilter(String... filters) {
+        return addFilter(true, filters);
+    }
+
+    /**
+     * Add a list of filters. Each filter can be defined with {type}:{key}[>|<|=]{value}
+     * Example:
+     *  FORMAT:GQ>10
+     *  INFO:DP>40
+     *  FILTER=lowGQ,lowDP
+     *
+     * @param mustPassAll      Must pass all filters
+     * @param filters          Filters
+     */
+    public VariantAvroFilters addFilter(boolean mustPassAll, String... filters) {
+        List<Predicate<Variant>> filtersList = new ArrayList<>(filters.length);
+
+        for (String filter : filters) {
+
+            String[] keyOpValue = splitOperator(filter);
+            String[] typeKey = keyOpValue[0].split(":");
+
+            String type;
+            String key;
+            if (typeKey.length == 1) {
+                if (typeKey[0].equals(FILTER) || typeKey[0].equals(QUAL)) {
+                    type = "FILE";
+                    key = typeKey[0];
+                } else {
+                    throw new IllegalArgumentException("");
+                }
+            } else {
+                type = typeKey[0];
+                key = typeKey[1];
+            }
+            String op = keyOpValue[1];
+            String value = keyOpValue[2];
+
+            Predicate<String> predicate;
+            switch (type) {
+                case "FORMAT":
+                    predicate = buildPredicate(op, value);
+                    filtersList.add(v -> filterSampleFormat(v, key, true, predicate));
+                    break;
+                case "INFO":
+                case "FILE":
+                    if (key.equals(FILTER)) {
+                        if (op.equals("=") || op.equals("==")) {
+                            Set<String> values;
+                            if (value.contains(",")) {
+                                values = new HashSet<>(Arrays.asList(value.split(",")));
+                            } else {
+                                values = Collections.singleton(value);
+                            }
+                            predicate = filterValue -> {
+                                for (String v : filterValue.split(VCFConstants.FILTER_CODE_SEPARATOR)) {
+                                    if (values.contains(v)) {
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            };
+
+                        } else {
+                            throw new IllegalArgumentException("Invalid operator " + op + " for FILE:FILTER");
+                        }
+                    } else {
+                        predicate = buildPredicate(op, value);
+                    }
+                    filtersList.add(v -> filterFileAttribute(v, key, predicate));
+                    break;
+                default:
+                    throw new IllegalArgumentException("unknown " + type);
+            }
+        }
+        addFilterList(filtersList, !mustPassAll);
+        return this;
+    }
+
     private boolean filterQuality(Variant variant, double minQual) {
         try {
             double qual;
@@ -119,7 +239,7 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
                 if (fileId == null || fileId.isEmpty()) {
                     for (StudyEntry studyEntry : variant.getStudies()) {
                         for (FileEntry fileEntry : studyEntry.getFiles()) {
-                            qual = Double.parseDouble(fileEntry.getAttributes().get("QUAL"));
+                            qual = Double.parseDouble(fileEntry.getAttributes().get(QUAL));
                             if (qual >= minQual) {
                                 return true;
                             }
@@ -128,7 +248,7 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
                 } else {
                     for (StudyEntry studyEntry : variant.getStudies()) {
                         if (studyEntry.getFile(fileId) != null) {
-                            qual = Double.parseDouble(studyEntry.getFile(fileId).getAttributes().get("QUAL"));
+                            qual = Double.parseDouble(studyEntry.getFile(fileId).getAttributes().get(QUAL));
                             if (qual >= minQual) {
                                 return true;
                             }
@@ -139,60 +259,55 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
                 StudyEntry studyEntry = variant.getStudy(datasetId);
                 if (fileId == null || fileId.isEmpty()) {
                     for (FileEntry fileEntry : studyEntry.getFiles()) {
-                        qual = Double.parseDouble(fileEntry.getAttributes().get("QUAL"));
+                        qual = Double.parseDouble(fileEntry.getAttributes().get(QUAL));
                         if (qual >= minQual) {
                             return true;
                         }
                     }
                 } else {
                     FileEntry fileEntry = studyEntry.getFile(fileId);
-                    qual = Double.parseDouble(fileEntry.getAttributes().get("QUAL"));
+                    qual = Double.parseDouble(fileEntry.getAttributes().get(QUAL));
                     return (qual >= minQual);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NumberFormatException e) {
+//            e.printStackTrace();
             return false;
         }
         return false;
     }
 
-    private boolean filterPass(Variant variant, String pass) {
-        try {
-            if (datasetId == null || datasetId.isEmpty()) {
-                if (fileId == null || fileId.isEmpty()) {
-                    for (StudyEntry studyEntry : variant.getStudies()) {
-                        for (FileEntry fileEntry : studyEntry.getFiles()) {
-                            if (inString(fileEntry.getAttributes().get("FILTER"), pass)) {
-                                return true;
-                            }
-                        }
-                    }
-                } else {
-                    for (StudyEntry studyEntry : variant.getStudies()) {
-                        if (studyEntry.getFile(fileId) != null) {
-                            if (inString(studyEntry.getFile(fileId).getAttributes().get("QUAL"), pass)) {
-                                return true;
-                            }
+    private boolean filterFilter(Variant variant, String pass) {
+        if (datasetId == null || datasetId.isEmpty()) {
+            if (fileId == null || fileId.isEmpty()) {
+                for (StudyEntry studyEntry : variant.getStudies()) {
+                    for (FileEntry fileEntry : studyEntry.getFiles()) {
+                        if (inString(fileEntry.getAttributes().get(FILTER), pass)) {
+                            return true;
                         }
                     }
                 }
             } else {
-                StudyEntry studyEntry = variant.getStudy(datasetId);
-                if (fileId == null || fileId.isEmpty()) {
-                    for (FileEntry fileEntry : studyEntry.getFiles()) {
-                        if (inString(fileEntry.getAttributes().get("QUAL"), pass)) {
+                for (StudyEntry studyEntry : variant.getStudies()) {
+                    if (studyEntry.getFile(fileId) != null) {
+                        if (inString(studyEntry.getFile(fileId).getAttributes().get(FILTER), pass)) {
                             return true;
                         }
                     }
-                } else {
-                    FileEntry fileEntry = studyEntry.getFile(fileId);
-                    return inString(fileEntry.getAttributes().get("QUAL"), pass);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        } else {
+            StudyEntry studyEntry = variant.getStudy(datasetId);
+            if (fileId == null || fileId.isEmpty()) {
+                for (FileEntry fileEntry : studyEntry.getFiles()) {
+                    if (inString(fileEntry.getAttributes().get(FILTER), pass)) {
+                        return true;
+                    }
+                }
+            } else {
+                FileEntry fileEntry = studyEntry.getFile(fileId);
+                return inString(fileEntry.getAttributes().get(FILTER), pass);
+            }
         }
         return false;
     }
@@ -207,5 +322,67 @@ public class VariantAvroFilters extends VariantFilters<Variant> {
             }
         }
         return false;
+    }
+
+    private boolean filterSampleFormat(Variant variant, String formatKey, boolean mustPassAll, Predicate<String> valueValidator) {
+        StudyEntry studyEntry = getStudyEntry(variant, datasetId);
+        Integer idx = studyEntry.getFormatPositions().get(formatKey);
+        if (idx == null || idx < 0) {
+            return valueValidator.test(null);
+        }
+        if (mustPassAll) {
+            for (List<String> data : studyEntry.getSamplesData()) {
+                String value = data.get(idx);
+                if (!valueValidator.test(value)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            for (List<String> data : studyEntry.getSamplesData()) {
+                String value = data.get(idx);
+                if (valueValidator.test(value)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private boolean filterFileAttribute(Variant variant, String attributeKey, Predicate<String> valueValidator) {
+        FileEntry fileEntry = getFileEntry(variant, datasetId, fileId);
+        String value = fileEntry == null ? null : fileEntry.getAttributes().get(attributeKey);
+
+        return valueValidator.test(value);
+    }
+
+    private static StudyEntry getStudyEntry(Variant variant, String studyId) {
+        StudyEntry studyEntry;
+        if (studyId == null) {
+            if (variant.getStudies().size() != 1) {
+                throw new IllegalArgumentException("Required one Study per variant. Found " + variant.getStudies().size()
+                        + " studies instead");
+            }
+            studyEntry = variant.getStudies().get(0);
+        } else {
+            studyEntry = variant.getStudy(studyId);
+        }
+        return studyEntry;
+    }
+
+    private static FileEntry getFileEntry(Variant variant, String studyId, String fileId) {
+        StudyEntry studyEntry = getStudyEntry(variant, studyId);
+        if (studyEntry == null) {
+            return null;
+        }
+        if (fileId == null) {
+            if (studyEntry.getFiles().size() != 1) {
+                throw new IllegalArgumentException("Required one File per variant. Found " + studyEntry.getFiles().size()
+                        + " files instead");
+            }
+            return studyEntry.getFiles().get(0);
+        } else {
+            return studyEntry.getFile(fileId);
+        }
     }
 }
