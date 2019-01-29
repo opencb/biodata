059d3cd4810eaf7e474c0a8e69c26a1b037dabfa
imedina <igmecas@gmail.com>
2019-01-29 13:29:17 +0000

    tools: add two implementations of reported variant to variant creators

--- biodata-tools/src/main/java/org/opencb/biodata/tools/clinical/TieringReportedVariantCreator.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/clinical/TieringReportedVariantCreator.java
@@ -1,35 +1,62 @@
-package org.opencb.biodata.models.clinical.utils;
+/*
+ * <!--
+ *   ~ Copyright 2015-2017 OpenCB
+ *   ~
+ *   ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *   ~ you may not use this file except in compliance with the License.
+ *   ~ You may obtain a copy of the License at
+ *   ~
+ *   ~     http://www.apache.org/licenses/LICENSE-2.0
+ *   ~
+ *   ~ Unless required by applicable law or agreed to in writing, software
+ *   ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *   ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   ~ See the License for the specific language governing permissions and
+ *   ~ limitations under the License.
+ *   -->
+ *
+ */
 
-import org.opencb.biodata.models.clinical.interpretation.*;
+package org.opencb.biodata.tools.clinical;
+
 import org.opencb.biodata.models.clinical.interpretation.ClinicalProperty.ModeOfInheritance;
 import org.opencb.biodata.models.clinical.interpretation.ClinicalProperty.Penetrance;
+import org.opencb.biodata.models.clinical.interpretation.*;
 import org.opencb.biodata.models.commons.Phenotype;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.avro.ConsequenceType;
 import org.opencb.commons.utils.ListUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
-public class VariantToReportedVariantConverter {
+public class TieringReportedVariantCreator extends ReportedVariantCreator {
 
-    public List<ReportedVariant> convert(List<Variant> variants, List<DiseasePanel> diseasePanels, Phenotype phenotype,
-                                         ModeOfInheritance moi, Penetrance penetrance) {
+    private List<DiseasePanel> diseasePanels;
+    private Phenotype phenotype;
+    private ModeOfInheritance modeOfInheritance;
+    private Penetrance penetrance;
 
-        Map<String, List<String>> genePanelMap = new HashMap<>();
-        if (ListUtils.isNotEmpty(diseasePanels)) {
-            for (DiseasePanel panel: diseasePanels) {
-                for (DiseasePanel.GenePanel genePanel : panel.getGenes()) {
-                    if (genePanel.getId() != null) {
-                        if (!genePanelMap.containsKey(genePanel.getId())) {
-                            genePanelMap.put(genePanel.getId(), new ArrayList<>());
-                        }
-                        genePanelMap.get(genePanel.getId()).add(panel.getId());
+    public TieringReportedVariantCreator(List<DiseasePanel> diseasePanels, ModeOfInheritance modeOfInheritance, Penetrance penetrance) {
+        this.diseasePanels = diseasePanels;
+        this.modeOfInheritance = modeOfInheritance;
+        this.penetrance = penetrance;
+    }
 
-                    }
-                    genePanelMap.put(genePanel.getId(), null);
-                }
-            }
-        }
+    public TieringReportedVariantCreator(List<DiseasePanel> diseasePanels, Phenotype phenotype, ModeOfInheritance modeOfInheritance,
+                                         Penetrance penetrance) {
+        this.diseasePanels = diseasePanels;
+        this.phenotype = phenotype;
+        this.modeOfInheritance = modeOfInheritance;
+        this.penetrance = penetrance;
+    }
+
+    @Override
+    public List<ReportedVariant> create(List<Variant> variants) {
+
+        Map<String, List<String>> geneToPanelIdMap = getGeneToPanelIdMap(diseasePanels);
 
         List<ReportedVariant> reportedVariants = new ArrayList<>();
         for (Variant variant: variants) {
@@ -39,18 +66,18 @@ public class VariantToReportedVariantConverter {
             if (variant.getAnnotation() != null && ListUtils.isNotEmpty(variant.getAnnotation().getConsequenceTypes())) {
                 // Create the reported event for each consequence type
                 for (ConsequenceType ct : variant.getAnnotation().getConsequenceTypes()) {
-                    if (genePanelMap.containsKey(ct.getEnsemblGeneId())) {
+                    if (geneToPanelIdMap.containsKey(ct.getEnsemblGeneId())) {
                         // Create the reported event for each gene panel
-                        for (String panelId: genePanelMap.get(ct.getEnsemblGeneId())) {
+                        for (String panelId: geneToPanelIdMap.get(ct.getEnsemblGeneId())) {
                             ReportedEvent reportedEvent = newReportedEvent(reportedVariant.getReportedEvents().size(),
-                            phenotype, ct, panelId, moi, penetrance, variant);
+                            phenotype, ct, panelId, modeOfInheritance, penetrance, variant);
 
                             // Add reported event to the reported variant
                             reportedVariant.getReportedEvents().add(reportedEvent);
                         }
                     } else {
                         ReportedEvent reportedEvent = newReportedEvent(reportedVariant.getReportedEvents().size(),
-                                phenotype, ct, null, moi, penetrance, variant);
+                                phenotype, ct, null, modeOfInheritance, penetrance, variant);
 
                         // Add reported event to the reported variant
                         reportedVariant.getReportedEvents().add(reportedEvent);
