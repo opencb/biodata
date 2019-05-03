1e2aa077dc943a778dd87637e532ae6038ef3d7f
Javi Lopez <javild@gmail.com>
2019-05-03 16:37:48 +0100

    feature-398-phased-variants: VariantVcfHtsjdkReader now allows reading batches without splitting sets of variants with the same PS in different batches. Also, allows combining VCF BND records into single Variant objects. Corresponding tests added.

--- biodata-tools/src/test/java/org/opencb/biodata/tools/variant/VariantVcfHtsjdkReaderTest.java
+++ biodata-tools/src/test/java/org/opencb/biodata/tools/variant/VariantVcfHtsjdkReaderTest.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created on 16/05/16
@@ -19,6 +20,159 @@ import static org.junit.Assert.assertEquals;
  * @author Jacobo Coll &lt;jacobo167@gmail.com&gt;
  */
 public class VariantVcfHtsjdkReaderTest {
+
+    @Test
+    public void missingPhaseTest() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/phasemissing.vcf");
+        VariantStudyMetadata metadata = new VariantFileMetadata("phasemissing.vcf", "2")
+                .toVariantStudyMetadata("sid");
+        VariantVcfHtsjdkReader reader = new VariantVcfHtsjdkReader(inputStream, metadata).setIgnorePhaseSet(false);
+        reader.open();
+        reader.pre();
+
+        // The third variant contains missing value - must not be included as part of the first batch
+        List<Variant> variantList = reader.read(2);
+        assertEquals(2, variantList.size());
+
+        Variant variant = getVariant(variantList, "rs1");
+        assertNotNull(variant);
+        variant = getVariant(variantList, "rs2");
+        assertNotNull(variant);
+
+        variantList = reader.read(2);
+        assertEquals(1, variantList.size());
+        variant = getVariant(variantList, "rs3");
+        assertNotNull(variant);
+
+        reader.post();
+        reader.close();
+    }
+
+    @Test
+    public void breakendParsingTest() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/bnd.vcf");
+        VariantStudyMetadata metadata = new VariantFileMetadata("bnd.vcf", "2")
+                .toVariantStudyMetadata("sid");
+        VariantVcfHtsjdkReader reader = new VariantVcfHtsjdkReader(inputStream, metadata)
+                .setIgnorePhaseSet(false)
+                .setCombineBreakends(true);
+        reader.open();
+        reader.pre();
+
+        List<Variant> variantList = reader.read(2);
+        // First batch will be empty since no paired BND are found, and both of them are saved aiming at finding their
+        // corresponding mates in posterior iterations
+        assertEquals(0, variantList.size());
+
+        // Second batch finds one BND without MATEID
+        variantList = reader.read(2);
+        assertEquals(2, variantList.size());
+
+        // This is a BND that should have been combined from two VCF lines (1st and 3rd)
+        Variant variant = getVariant(variantList, "MantaBND:16:1:2:1:0:0:1");
+        assertNotNull(variant);
+        assertEquals(Integer.valueOf(245657), variant.getSv().getCiStartLeft());
+        assertEquals(Integer.valueOf(245697), variant.getSv().getCiStartRight());
+        assertNotNull(variant.getSv().getBreakend());
+        assertNotNull(variant.getSv().getBreakend().getMate());
+        assertEquals("1", variant.getSv().getBreakend().getMate().getChromosome());
+        assertEquals(Integer.valueOf(797179), variant.getSv().getBreakend().getMate().getPosition());
+        assertEquals(Integer.valueOf(797179), variant.getSv().getBreakend().getMate().getCiPositionLeft());
+        assertEquals(Integer.valueOf(797219), variant.getSv().getBreakend().getMate().getCiPositionRight());
+
+        // This is a BND without MATEID field
+        variant = getVariant(variantList, "MantaBND:16:1:2:1:0:0:2");
+        assertNotNull(variant);
+
+        // Third batch reads two non BND
+        variantList = reader.read(2);
+        assertEquals(2, variantList.size());
+
+        // CNV must have been returned
+        variant = getVariant(variantList, "Canvas:GAIN:1:815030:824821");
+        assertNotNull(variant);
+
+        // SNV must have been returned
+        variant = getVariant(variantList, "rstest");
+        assertNotNull(variant);
+
+        // Last batch reaches end of file and drains
+        variantList = reader.read(2);
+        assertEquals(1, variantList.size());
+
+        // Singleton with mate BND specified in VCF line though, must have been drained
+        variant = getVariant(variantList, "MantaBND:16:1:2:0:0:0:0");
+        assertNotNull(variant);
+
+
+
+
+        reader.post();
+        reader.close();
+    }
+
+    private Variant getVariant(List<Variant> variantList, String id) {
+        for (Variant variant : variantList) {
+            if (variant.getId().equals(id)) {
+                return variant;
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void batchSplitsPhaseTest() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/phase.vcf");
+        VariantStudyMetadata metadata = new VariantFileMetadata("phase.vcf", "2")
+                .toVariantStudyMetadata("sid");
+        VariantVcfHtsjdkReader reader = new VariantVcfHtsjdkReader(inputStream, metadata).setIgnorePhaseSet(false);
+        reader.open();
+        reader.pre();
+
+        List<Variant> variantList = reader.read(2);
+        assertEquals(3, variantList.size());
+        assertEquals("99166",
+                variantList.get(1).getStudies().get(0).getSampleData("S1", "PS"));
+        assertEquals("99166",
+                variantList.get(2).getStudies().get(0).getSampleData("S1", "PS"));
+
+        reader.post();
+        reader.close();
+    }
+
+    @Test
+    public void batchSplitsPhaseIgnorePhaseTest() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/phase.vcf");
+        VariantStudyMetadata metadata = new VariantFileMetadata("phase.vcf", "2")
+                .toVariantStudyMetadata("sid");
+        VariantVcfHtsjdkReader reader = new VariantVcfHtsjdkReader(inputStream, metadata).setIgnorePhaseSet(true);
+        reader.open();
+        reader.pre();
+
+        List<Variant> variantList = reader.read(2);
+        assertEquals(2, variantList.size());
+
+        Variant variant = variantList.get(1);
+        assertEquals("1", variant.getChromosome());
+        assertEquals(Integer.valueOf(99166), variant.getStart());
+        assertEquals("C", variant.getReference());
+        assertEquals("T", variant.getAlternate());
+        assertEquals("99166",
+                variant.getStudies().get(0).getSampleData("S1", "PS"));
+
+        variantList = reader.read(2);
+        assertEquals(1, variantList.size());
+        variant = variantList.get(0);
+        assertEquals("1", variant.getChromosome());
+        assertEquals(Integer.valueOf(99180), variant.getStart());
+        assertEquals("T", variant.getReference());
+        assertEquals("C", variant.getAlternate());
+        assertEquals("99166",
+                variant.getStudies().get(0).getSampleData("S1", "PS"));
+
+        reader.post();
+        reader.close();
+    }
 
     @Test
     public void readFileTest() throws Exception {
