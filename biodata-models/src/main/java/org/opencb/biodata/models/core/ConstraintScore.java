fd2a371bc592ac0f8109db68069143e5dafd323b
Julie Sullivan <julie.sullivan@genomicsengland.co.uk>
2019-10-09 15:48:02 +0100

    Add constraint scores to the data model

---
+++ biodata-models/src/main/java/org/opencb/biodata/models/core/ConstraintScore.java
@@ -0,0 +1,68 @@
+/*
+ * <!--
+ *   ~ Copyright 2015-2017 OpenCB
+ *   ~
+ *   ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *   ~ you may not use this file except in compliance with the License.
+ *   ~ You may obtain a copy of the License at
+ *   ~
+ *   ~     http://www.apache.org/licenses/LICENSE-2.0
+ *   ~
+ *   ~ Unless required by applicable law or agreed to in writing, software
+ *   ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *   ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   ~ See the License for the specific language governing permissions and
+ *   ~ limitations under the License.
+ *   -->
+ *
+ */
+
+package org.opencb.biodata.models.core;
+
+public class ConstraintScore {
+
+    private String source;
+    private String method;
+    private String name;
+    private Double value;
+
+    public ConstraintScore(String source, String method, String name, Double value) {
+        this.source = source;
+        this.method = method;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+}
