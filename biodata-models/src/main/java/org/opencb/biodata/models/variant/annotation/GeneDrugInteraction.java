cc97340ea88fc5452561dfe51b05dd0176fa5071
Julie Sullivan <julie.sullivan@gmail.com>
2020-04-23 10:54:51 +0100

    update to new data model for new data file. Doesn't matter really because I don't think this class is being used anywhere

--- biodata-models/src/main/java/org/opencb/biodata/models/variant/annotation/GeneDrugInteraction.java
+++ biodata-models/src/main/java/org/opencb/biodata/models/variant/annotation/GeneDrugInteraction.java
@@ -19,19 +19,37 @@
 
 package org.opencb.biodata.models.variant.annotation;
 
+import java.util.List;
+
 /**
  * Created by fjlopez on 16/07/15.
  */
 public class GeneDrugInteraction {
 
     private String geneName;
-    private String drugName;
     private String source;
+    private String interactionType;
+    private String drugName;
+    private String chemblId;
+    private List<String> publications;
+    @Deprecated
     private String studyType;
+    @Deprecated
     private String type;
 
     public GeneDrugInteraction() {};
 
+    public GeneDrugInteraction(String geneName, String source, String interactionType, String drugName, String chemblId,
+                               List<String> publications) {
+        this.geneName = geneName;
+        this.source = source;
+        this.interactionType = interactionType;
+        this.drugName = drugName;
+        this.chemblId = chemblId;
+        this.publications = publications;
+    }
+
+    @Deprecated
     public GeneDrugInteraction(String geneName, String drugName, String source, String studyType, String type) {
         this.geneName = geneName;
         this.drugName = drugName;
@@ -40,39 +58,88 @@ public class GeneDrugInteraction {
         this.type = type;
     }
 
-    public String getGeneName() { return geneName; }
-
-    public void setGeneName(String geneName) { this.geneName = geneName; }
-
-    public String getDrugName() {
-        return drugName;
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GeneDrugInteraction{");
+        sb.append("geneName='").append(geneName).append('\'');
+        sb.append(", source='").append(source).append('\'');
+        sb.append(", interactionType='").append(interactionType).append('\'');
+        sb.append(", drugName='").append(drugName).append('\'');
+        sb.append(", chemblId='").append(chemblId).append('\'');
+        sb.append(", publications=").append(publications);
+        sb.append('}');
+        return sb.toString();
     }
 
-    public void setDrugName(String drugName) {
-        this.drugName = drugName;
+    public String getGeneName() {
+        return geneName;
+    }
+
+    public GeneDrugInteraction setGeneName(String geneName) {
+        this.geneName = geneName;
+        return this;
     }
 
     public String getSource() {
         return source;
     }
 
-    public void setSource(String source) {
+    public GeneDrugInteraction setSource(String source) {
         this.source = source;
+        return this;
+    }
+
+    public String getInteractionType() {
+        return interactionType;
+    }
+
+    public GeneDrugInteraction setInteractionType(String interactionType) {
+        this.interactionType = interactionType;
+        return this;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public GeneDrugInteraction setDrugName(String drugName) {
+        this.drugName = drugName;
+        return this;
+    }
+
+    public String getChemblId() {
+        return chemblId;
+    }
+
+    public GeneDrugInteraction setChemblId(String chemblId) {
+        this.chemblId = chemblId;
+        return this;
+    }
+
+    public List<String> getPublications() {
+        return publications;
+    }
+
+    public GeneDrugInteraction setPublications(List<String> publications) {
+        this.publications = publications;
+        return this;
     }
 
     public String getStudyType() {
         return studyType;
     }
 
-    public void setStudyType(String studyType) {
+    public GeneDrugInteraction setStudyType(String studyType) {
         this.studyType = studyType;
+        return this;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public GeneDrugInteraction setType(String type) {
         this.type = type;
+        return this;
     }
 }
