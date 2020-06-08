6f4903c7df22c2d3422392c8cdfbe10b9dcc68de
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2020-06-08 18:09:50 +0200

    models: add new fields to transcript coverage stats model

--- biodata-models/src/main/java/org/opencb/biodata/models/alignment/TranscriptCoverageStats.java
+++ biodata-models/src/main/java/org/opencb/biodata/models/alignment/TranscriptCoverageStats.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class TranscriptCoverageStats {
 
-    private String transcriptId;
+    private String id;
+    private String name;
+    private String biotype;
+    private String chromosome;
+    private int start;
+    private int end;
     private int length; // as the sum of lengths of the exons
     private double[] depths; // % coverage for 1x, 5x, 10x, 15x, 20x, 25x, 30x, 40x, 50x, 60x
     private int lowCoverageThreshold;
@@ -15,9 +20,14 @@ public class TranscriptCoverageStats {
         depths = new double[10];
     }
 
-    public TranscriptCoverageStats(String transcriptId, int length, double[] depths, int lowCoverageThreshold,
-                                   List<LowCoverageRegion> lowCoverageRegions) {
-        this.transcriptId = transcriptId;
+    public TranscriptCoverageStats(String id, String name, String biotype, String chromosome, int start, int end, int length,
+                                   double[] depths, int lowCoverageThreshold, List<LowCoverageRegion> lowCoverageRegions) {
+        this.id = id;
+        this.name = name;
+        this.biotype = biotype;
+        this.chromosome = chromosome;
+        this.start = start;
+        this.end = end;
         this.length = length;
         this.depths = depths;
         this.lowCoverageThreshold = lowCoverageThreshold;
@@ -27,7 +37,12 @@ public class TranscriptCoverageStats {
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("TranscriptCoverageStats{");
-        sb.append("transcriptId='").append(transcriptId).append('\'');
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", biotype='").append(biotype).append('\'');
+        sb.append(", chromosome='").append(chromosome).append('\'');
+        sb.append(", start=").append(start);
+        sb.append(", end=").append(end);
         sb.append(", length=").append(length);
         sb.append(", depths=").append(Arrays.toString(depths));
         sb.append(", lowCoverageThreshold=").append(lowCoverageThreshold);
@@ -36,12 +51,57 @@ public class TranscriptCoverageStats {
         return sb.toString();
     }
 
-    public String getTranscriptId() {
-        return transcriptId;
+    public String getId() {
+        return id;
     }
 
-    public TranscriptCoverageStats setTranscriptId(String transcriptId) {
-        this.transcriptId = transcriptId;
+    public TranscriptCoverageStats setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TranscriptCoverageStats setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getBiotype() {
+        return biotype;
+    }
+
+    public TranscriptCoverageStats setBiotype(String biotype) {
+        this.biotype = biotype;
+        return this;
+    }
+
+    public String getChromosome() {
+        return chromosome;
+    }
+
+    public TranscriptCoverageStats setChromosome(String chromosome) {
+        this.chromosome = chromosome;
+        return this;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public TranscriptCoverageStats setStart(int start) {
+        this.start = start;
+        return this;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public TranscriptCoverageStats setEnd(int end) {
+        this.end = end;
         return this;
     }
 
