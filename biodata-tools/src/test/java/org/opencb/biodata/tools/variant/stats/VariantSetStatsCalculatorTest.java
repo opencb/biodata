be0669a9e9f9582bdd6bc41086a2412a91aad630
Jacobo Coll Moragón <jacobo167@gmail.com>
2021-09-02 10:33:20 +0100

    tools: Add genotypeCount to VariantSetStats opencb/opencga#1817

--- biodata-tools/src/test/java/org/opencb/biodata/tools/variant/stats/VariantSetStatsCalculatorTest.java
+++ biodata-tools/src/test/java/org/opencb/biodata/tools/variant/stats/VariantSetStatsCalculatorTest.java
@@ -33,6 +33,11 @@ public class VariantSetStatsCalculatorTest {
     private VariantReader reader;
     private VariantStudyMetadata studyMetadata;
 
+    private int samplesPerFile = 5;
+    private int files = 2;
+    private int variants = 4;
+    private double passVariants = 3/4f;
+
     @Before
     public void setUp() throws Exception {
         List<Variant> variants = new ArrayList<>();
@@ -43,8 +48,8 @@ public class VariantSetStatsCalculatorTest {
         studyMetadata.setId(STUDY_ID);
         studyMetadata.setFiles(Arrays.asList(
                 getVariantFileMetadata(header, 1).getImpl(),
-                getVariantFileMetadata(header, 2).getImpl(),
-                getVariantFileMetadata(header, 3).getImpl()
+                getVariantFileMetadata(header, 2).getImpl()
+//                getVariantFileMetadata(header, 3).getImpl()
         ));
 
         studyMetadata.setAggregatedHeader(header);
@@ -81,7 +86,7 @@ public class VariantSetStatsCalculatorTest {
 
     protected VariantFileMetadata getVariantFileMetadata(VariantFileHeader header, int fileId) {
         VariantFileMetadata fileMetadata = new VariantFileMetadata("F" + fileId, "");
-        fileMetadata.setSampleIds(Arrays.asList(buildSampleId(fileId, 1), buildSampleId(fileId, 2)));
+        fileMetadata.setSampleIds(Arrays.asList(buildSampleId(fileId, 1), buildSampleId(fileId, 2), buildSampleId(fileId, 3), buildSampleId(fileId, 4), buildSampleId(fileId, 5)));
         fileMetadata.setHeader(header);
         return fileMetadata;
     }
@@ -90,11 +95,12 @@ public class VariantSetStatsCalculatorTest {
         Variant variant = new Variant(var);
         StudyEntry studyEntry = new StudyEntry(STUDY_ID);
         studyEntry.setFiles(Arrays.asList(files));
-        studyEntry.setSampleDataKeys(Arrays.asList("GT"));
+        studyEntry.setSampleDataKeys(Collections.singletonList("GT"));
         for (FileEntry file : files) {
-            Integer fileId = Integer.valueOf(file.getFileId().substring(1));
-            for (Integer i = 0; i < fileId; i++) {
-                studyEntry.addSampleData(buildSampleId(fileId, i), Collections.singletonList("0/0"));
+            int fileId = Integer.parseInt(file.getFileId().substring(1));
+            for (int i = 1; i <= 5; i++) {
+                String gt = (studyEntry.getSamples().size() % 4 == 0 ? 0 : 1) + "/" + (studyEntry.getSamples().size() % 2);
+                studyEntry.addSampleData(buildSampleId(fileId, i), Collections.singletonList(gt));
             }
         }
         variant.addStudyEntry(studyEntry);
@@ -125,11 +131,18 @@ public class VariantSetStatsCalculatorTest {
         VariantSetStats stats = calculateStats(statsTask);
 
         System.out.println("stats = " + stats);
+
+        assertEquals(variants, stats.getVariantCount().intValue());
+        assertEquals(((int) (files * variants * passVariants)), stats.getFilterCount().get("PASS").intValue());
+        assertEquals(files, stats.getFilesCount().intValue());
+        assertEquals(files * samplesPerFile, stats.getSampleCount().intValue());
+        assertEquals(files * samplesPerFile * variants, stats.getGenotypeCount().values().stream().reduce(Long::sum).orElse(0L).intValue());
     }
 
     @Test
     public void testFileStats() throws Exception {
-        VariantSetStatsCalculator statsTask = new VariantSetStatsCalculator(STUDY_ID, new VariantFileMetadata(studyMetadata.getFiles().get(0)));
+        VariantSetStatsCalculator statsTask = new VariantSetStatsCalculator(studyMetadata,
+                Collections.singletonList(studyMetadata.getFiles().get(0).getId()));
         VariantSetStats stats = calculateStats(statsTask);
 
         System.out.println("stats = " + stats);
@@ -141,9 +154,11 @@ public class VariantSetStatsCalculatorTest {
         }
         stDev = Math.sqrt(stDev / quals.length);
 
-        assertEquals(4, stats.getVariantCount().intValue());
-        assertEquals(3, stats.getFilterCount().get("PASS").intValue());
-        assertEquals(2, stats.getSampleCount().intValue());
+        assertEquals(variants, stats.getVariantCount().intValue());
+        assertEquals(((int) (variants * passVariants)), stats.getFilterCount().get("PASS").intValue());
+        assertEquals(1, stats.getFilesCount().intValue());
+        assertEquals(samplesPerFile, stats.getSampleCount().intValue());
+        assertEquals(samplesPerFile * variants, stats.getGenotypeCount().values().stream().reduce(Long::sum).orElse(0L).intValue());
 
         assertEquals(mean, stats.getQualityAvg(), 0.000001);
         assertEquals(stDev, stats.getQualityStdDev(), 0.000001);
