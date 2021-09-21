bf53a76682f1167a74fd0303701a56e5c6bef128
Jacobo Coll Moragón <jacobo167@gmail.com>
2021-09-21 18:49:19 +0100

    tools: Some fixes for EnsemblVepOutputWriter

--- biodata-tools/src/main/java/org/opencb/biodata/tools/variant/writers/EnsemblVepOutputWriter.java
+++ biodata-tools/src/main/java/org/opencb/biodata/tools/variant/writers/EnsemblVepOutputWriter.java
@@ -27,20 +27,27 @@ import org.opencb.biodata.models.variant.avro.SequenceOntologyTerm;
 import org.opencb.commons.io.DataWriter;
 
 import java.io.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
+/**
+ *
+ * @link http://grch37.ensembl.org/info/docs/tools/vep/vep_formats.html#tab
+ */
 public class EnsemblVepOutputWriter implements DataWriter<Variant> {
 
     private final Writer dataOutputStream;
     private final boolean closeStream;
     private int writtenVariants;
 
-    protected EnsemblVepOutputWriter(OutputStream os) {
+    public EnsemblVepOutputWriter(OutputStream os) {
         this(new OutputStreamWriter(os));
     }
 
-    protected EnsemblVepOutputWriter(Writer dataOutputStream) {
+    public EnsemblVepOutputWriter(Writer dataOutputStream) {
         this.dataOutputStream = dataOutputStream;
         this.closeStream = false;
     }
@@ -48,6 +55,69 @@ public class EnsemblVepOutputWriter implements DataWriter<Variant> {
     @Override
     public boolean pre() {
         writtenVariants = 0;
+        try {
+//            dataOutputStream.write("## ENSEMBL VARIANT EFFECT PREDICTOR v104.0");
+            dataOutputStream.write("## Output produced at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date.from(Instant.now())) + "\n");
+
+            // Metadata description
+//            dataOutputStream.write("## Connected to homo_sapiens_core_104_38 on ensembldb.ensembl.org");
+//            dataOutputStream.write("## Using cache in /homes/user/.vep/homo_sapiens/104_GRCh38");
+//            dataOutputStream.write("## Using API version 104, DB version 104");
+//            dataOutputStream.write("## polyphen version 2.2.2");
+//            dataOutputStream.write("## sift version sift5.2.2");
+//            dataOutputStream.write("## COSMIC version 78");
+//            dataOutputStream.write("## ESP version 20141103");
+//            dataOutputStream.write("## gencode version GENCODE 25");
+//            dataOutputStream.write("## genebuild version 2014-07");
+//            dataOutputStream.write("## HGMD-PUBLIC version 20162");
+//            dataOutputStream.write("## regbuild version 16");
+//            dataOutputStream.write("## assembly version GRCh38.p7");
+//            dataOutputStream.write("## ClinVar version 201610");
+//            dataOutputStream.write("## dbSNP version 147");
+
+            // Columns
+            dataOutputStream.write("## Column descriptions:\n");
+            dataOutputStream.write("## Uploaded_variation : Identifier of uploaded variant as chromosome_start_alleles\n");
+            dataOutputStream.write("## Location : Location of variant in standard coordinate format (chr:start or chr:start-end)\n");
+            dataOutputStream.write("## Allele : The variant allele used to calculate the consequence\n");
+            dataOutputStream.write("## Gene : Stable ID of affected gene\n");
+            dataOutputStream.write("## Feature : Stable ID of feature\n");
+            dataOutputStream.write("## Feature_type : Type of feature - Transcript, RegulatoryFeature or MotifFeature\n");
+            dataOutputStream.write("## Consequence : Consequence type\n");
+            dataOutputStream.write("## cDNA_position : Relative position of base pair in cDNA sequence\n");
+            dataOutputStream.write("## CDS_position : Relative position of base pair in coding sequence\n");
+            dataOutputStream.write("## Protein_position : Relative position of amino acid in protein\n");
+            dataOutputStream.write("## Amino_acids : Reference and variant amino acids\n");
+            dataOutputStream.write("## Codons : Reference and variant codon sequence\n");
+            dataOutputStream.write("## Existing_variation : Identifier(s) of co-located known variants\n");
+            dataOutputStream.write("## Extra column keys: This column contains extra information as key=value pairs separated by \";\"\n");
+            // Other columns. None extra columns are being used right now
+//            dataOutputStream.write("## REF_ALLELE : the reference allele\n");
+//            dataOutputStream.write("## HGVSc : the HGVS coding sequence name\n");
+//            dataOutputStream.write("## HGVSp : the HGVS protein sequence name\n");
+//            dataOutputStream.write("## HGVSg : the HGVS genomic sequence name\n");
+//            dataOutputStream.write("## IMPACT : Subjective impact classification of consequence type\n");
+//            dataOutputStream.write("## DISTANCE : Shortest distance from variant to transcript\n");
+//            dataOutputStream.write("## STRAND : Strand of the feature (1/-1)\n");
+//            dataOutputStream.write("## FLAGS : Transcript quality flags\n");
+            dataOutputStream.write(buildLine(new StringBuilder(),
+                    "#Uploaded_variation",
+                    "Location",
+                    "Allele", "Gene",
+                    "Feature",
+                    "Feature_type",
+                    "Consequence",
+                    "cDNA_position",
+                    "CDS_position",
+                    "Protein_position",
+                    "Amino_acids",
+                    "Codons",
+                    "Existing_variation",
+                    "EXTRA"
+            ).toString());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
         return true;
     }
 
@@ -66,7 +136,11 @@ public class EnsemblVepOutputWriter implements DataWriter<Variant> {
         String gene, feature, featureType, consequence, cdnaPosition, cdsPosiiton, proteinPosition, aaChange, codonChange;
         Map<String, String> extra;
         try {
-            variantId = variant.getChromosome() + ":" + variant.getStart() + ":" + variant.getReference() + ":" + variant.getAlternate();
+//            variantId = variant.getChromosome() + ":" + variant.getStart() + ":" + variant.getReference() + ":" + variant.getAlternate();
+            variantId = variant.getChromosome() + "_" + variant.getStart() + "_"
+                    + (variant.getReference().isEmpty() ? "-" : variant.getReference())
+                    + "/"
+                    + (variant.getAlternate().isEmpty() ? "-" : variant.getAlternate());
             location = variant.getChromosome() + ":" + variant.getStart() + "-" + variant.getEnd();
             alternateAllele = variant.getAlternate();
 
@@ -78,7 +152,7 @@ public class EnsemblVepOutputWriter implements DataWriter<Variant> {
                         featureType = "transcript";
                         cdnaPosition = String.valueOf(consequenceType.getCdnaPosition());
                         cdsPosiiton = String.valueOf(consequenceType.getCdsPosition());
-                        codonChange = consequenceType.getCodon();
+                        codonChange = StringUtils.isEmpty(consequenceType.getCodon()) ? "-" : consequenceType.getCodon();
                         if (consequenceType.getProteinVariantAnnotation() != null) {
                             proteinPosition = String.valueOf(consequenceType.getProteinVariantAnnotation().getPosition());
                             aaChange = consequenceType.getProteinVariantAnnotation().getAlternate();
@@ -97,17 +171,11 @@ public class EnsemblVepOutputWriter implements DataWriter<Variant> {
                     }
                     for (SequenceOntologyTerm sequenceOntologyTerm : consequenceType.getSequenceOntologyTerms()) {
                         consequence = sequenceOntologyTerm.getName();
-                        sb.append(variantId).append(location).append(alternateAllele)
-                                .append(gene).append(feature).append(featureType).append(consequence)
-                                .append(cdnaPosition).append(cdsPosiiton).append(proteinPosition).append(aaChange).append(codonChange)
-                                .append("-").append("-").append("\n");
+                        buildLine(sb, variantId, location, alternateAllele, gene, feature, featureType, consequence, cdnaPosition, cdsPosiiton, proteinPosition, aaChange, codonChange, "-", "-");
                     }
                 }
             } else {
-                sb.append(variantId).append(location).append(alternateAllele)
-                        .append("-").append("-").append("-").append("-")
-                        .append("-").append("-").append("-").append("-").append("-")
-                        .append("-").append("-").append("\n");
+                buildLine(sb, variantId, location, alternateAllele, "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-");
             }
 
             dataOutputStream.write(sb.toString());
@@ -117,6 +185,29 @@ public class EnsemblVepOutputWriter implements DataWriter<Variant> {
 
         writtenVariants++;
         return true;
+    }
+
+    private StringBuilder buildLine(StringBuilder sb, String variantId, String location, String alternateAllele, String gene, String feature,
+                                    String featureType, String consequence, String cdnaPosition, String cdsPosiiton, String proteinPosition,
+                                    String aaChange, String codonChange, String existingVariant, String extra) {
+        return sb
+                .append(variantId).append('\t')
+                .append(location).append('\t')
+                .append(alternateAllele).append('\t')
+
+                .append(gene).append('\t')
+                .append(feature).append('\t')
+                .append(featureType).append('\t')
+                .append(consequence).append('\t')
+
+                .append(cdnaPosition).append('\t')
+                .append(cdsPosiiton).append('\t')
+                .append(proteinPosition).append('\t')
+                .append(aaChange).append('\t')
+                .append(codonChange).append('\t')
+
+                .append(existingVariant).append('\t')
+                .append(extra).append('\n');
     }
 
     @Override
