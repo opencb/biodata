b264cdf1a352dea28cc2a8708cfb5121c09de0de
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2025-11-07 12:23:16 +0100

    Adapt workflows to the new cache #TASK-7809

--- .github/workflows/scripts/get_same_branch.sh
+++ .github/workflows/scripts/get_same_branch.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
 BRANCH_NAME=$1
+DEPENDENCIES_SHA=${DEPENDENCIES_SHA:-""}
+WORKSPACE=${WORKSPACE:-/home/runner/work/}
 
-if [[ -z $BRANCH_NAME  ]]; then
+if [[ -z "$BRANCH_NAME"  ]]; then
   echo "The first parameter is mandatory and must be a valid branch name."
   exit 1
 fi
 
-if [[ $BRANCH_NAME != "TASK-"*   ]]; then
-  echo "No need to check dependencies."
-  exit 0
-fi
-
-function install(){
+function checkout(){
   local REPO=$1
-  cd /home/runner/work/ || exit 2
+  echo "::group::Installing '$REPO' project from branch $BRANCH_NAME"
+  cd "${WORKSPACE}" || exit 2
   git clone https://github.com/opencb/"$REPO".git -b "$BRANCH_NAME"
   if [ -d "./$REPO" ]; then
     cd "$REPO" || exit 2
+    DEPENDENCIES_SHA=${DEPENDENCIES_SHA}:$(git rev-parse HEAD)
     echo "Branch name $BRANCH_NAME already exists."
-    mvn clean install -DskipTests
   else
-    echo "$CURRENT Branch is NOT EQUALS $BRANCH_NAME "
+    echo "Branch name $BRANCH_NAME does not exist in $REPO repository. Skipping installation."
   fi
+  echo "::endgroup::"
 }
 
-install "java-common-libs"
+checkout "java-common-libs"
+checkout "biodata"
+
+## Apply sha1 to DEPENDENCIES_SHA if contains `:`
+if [[ "$DEPENDENCIES_SHA" == *":"* ]]; then
+  DEPENDENCIES_SHA=$(echo -n "$DEPENDENCIES_SHA" | sha1sum | awk '{print $1}')
+fi
+
+## Export DEPENDENCIES_SHA as github output
+echo "dependencies_sha=$DEPENDENCIES_SHA" >> "$GITHUB_OUTPUT"
